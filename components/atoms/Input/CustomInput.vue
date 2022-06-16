cf67b9d8455c3f15599eda17834ca915c35208d8
JaehonDev <println@kakao.com>
2022-06-16 09:05:07 +0900

    init

---
+++ components/atoms/Input/CustomInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type='text'/>
+    <span>1</span>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "CustomInput"
+}
+</script>
+
+<style scoped>
+
+</style>
