c1cb6720e257386a524a8780ac144830c5eef7d3
jaehong <println@kakao.com>
2022-06-24 10:50:28 +0900

    Create Header vue

--- components/Organism/Header/Header.vue
+++ components/Organism/Header/Header.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    Header
-  </div>
+  <v-app-bar
+    class="xidps-header bg-light-navy-blue"
+    fixed
+    app
+  >
+    <v-app-bar-nav-icon/>
+    <v-btn
+      icon
+    >
+    </v-btn>
+    <v-btn
+      icon
+    >
+      <v-icon>mdi-application</v-icon>
+    </v-btn>
+    <v-btn
+      icon
+    >
+      <v-icon>mdi-minus</v-icon>
+    </v-btn>
+    <v-spacer />
+    <v-btn
+      icon
+    >
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
