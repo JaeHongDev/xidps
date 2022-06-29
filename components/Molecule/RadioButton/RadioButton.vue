f0aaeb2681715c1b095476e3e07680ddf76d4b96
jaehong <println@kakao.com>
2022-06-30 08:56:08 +0900

    Update ReadioButton Story
    RadioButton Component의 스타일 변경

--- components/Molecule/RadioButton/RadioButton.vue
+++ components/Molecule/RadioButton/RadioButton.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="radio-button-wrap">
-    <v-btn  data-test="button" v-for="(item,index) in lists " :key="index" :class="checkedStyle(index)" class="fw-bold"
-           @click="select(item,index)"> {{ item }}
+    <v-btn
+      data-test="button"
+      v-for="(item,index) in lists "
+      :key="index"
+      :class="checkedStyle(index)" class="fw-bold"
+           @click="select(item,index)">
+      {{ item }}
     </v-btn>
   </div>
 </template>
@@ -9,21 +14,21 @@
 <script>
 export default {
   name: "RadioButton",
-  data(){
-    return{
+  data() {
+    return {
       selectedIndex: -1
     }
   },
-  props:{
+  props: {
     lists: []
   },
   methods: {
-    select(item,index) {
+    select(item, index) {
       if (index === this.selectedIndex) return; // 현재 선택한 버튼과 동일한 버튼의 경우
       this.selectedIndex = index;
-      this.$emit("call:Api",item);
+      this.$emit("call:Api", item);
     },
-    checkedStyle(index){
+    checkedStyle(index) {
       return index === this.selectedIndex ? 'checked' : 'unchecked'
     },
 
@@ -32,25 +37,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.radio-button-wrap{
-  width:250px;
+.radio-button-wrap {
+  width: 250px;
   display: flex;
   flex-wrap: wrap;
-  .v-btn{
+
+  .v-btn {
     max-width: 24px !important;
-    max-height:24px !important;
-    min-height:24px !important;
+    max-height: 24px !important;
+    min-height: 24px !important;
     min-width: 24px !important;
-    margin:5px;
-    padding:0px !important;
+    margin: 5px;
+    padding: 0px !important;
   }
-  .unchecked{
-    background-color:#e1e1e1 !important;
-    color:#707070 !important;
+
+  .unchecked {
+    background-color: #e1e1e1 !important;
+    color: $warm-grey !important;
 
   }
+
   .checked {
-    background-color: indigo !important;
+    background-color: $light-navy-blue !important;
     color: white !important;
   }
 }
