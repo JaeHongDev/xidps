b2c4738ad30dc0ea57beb3948a2355aa8212ef01
jaehong <println@kakao.com>
2022-06-30 08:46:44 +0900

    Remove unused component and stories
    사용하지 않는 컴포넌트 및 스토리 파일 삭제하기

--- components/atoms/Input/CustomInput.stories.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title:"custom Input"
-};
-export const Default = (props) => {
-  console.log(props);
-  return `<custom-input>11</custom-input>`;
-}
-
-export const Other = () =>`<custom-input>other</custom-input>`;
