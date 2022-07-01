6d539ac12c78dbde8f7b30f629363ad9cdaf6423
jaehong <println@kakao.com>
2022-07-01 14:00:06 +0900

    create searchTextBox Component
    searchTextBox component 추가 및 스토리 생성

--- components/Molecule/SearchTextBox/SearchTextBox.vue
+++ components/Molecule/SearchTextBox/SearchTextBox.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    searchTextBox
-  </div>
+  <v-container>
+  <v-text-field
+    v-model="message"
+    outlined
+    clearable
+    dense
+    type="text"
+    hide-details
+    height="30px"
+    class="xidps-search-text"
+  >
+    <template v-slot:append>
+      <v-btn icon  width="24" height="24">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </template>
+  </v-text-field>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "SearchTextBox"
+  name: "SearchTextBox",
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.v-input__control{
+  min-height: 30px !important;
+}
+.xidps-search-bar{
+  font-size: $fs8 !important;
+}
 </style>
