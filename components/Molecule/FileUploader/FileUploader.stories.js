a2648d438b6c088c7a30cb76dda7356c44d69b34
jaehong <println@kakao.com>
2022-07-06 15:14:56 +0900

    Create FileUploader
    FileUploader 컴포넌트 및 스토리 파일 생성 , 디자인 추가

--- components/Molecule/FileUploader/FileUploader.stories.js
+++ components/Molecule/FileUploader/FileUploader.stories.js
@@ -1,15 +1,14 @@
 import FileUploader from "@/components/Molecule/FileUploader/FileUploader";
-
+import {VApp} from "vuetify/lib/components";
 export default{
   title:"Molecule/FileUploader",
   component:FileUploader
 }
 
 const Template = (args,{argsType}) =>({
-
   components: {FileUploader},
   props:Object.keys(args),
-  template: `<file-uploader v-bind="$props"></file-uploader>`
+  template: `<v-app><file-uploader v-bind="$props"></file-uploader></v-app>`
 })
 
 export const sample = Template.bind({})
--- components/Molecule/FileUploader/FileUploader.vue
+++ components/Molecule/FileUploader/FileUploader.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>File Uploader</div>
+  <div class="xidps-file-container">
+    <div class="xidps-file-result">
+      <div class="xidps-file-list"></div>
+      <div class="xidps-file-type">
+        <span class="fs-7 fw-regular">0KB/0KB</span>
+      </div>
+    </div>
+    <v-btn outlined class="xidps-file-btn" type="file"><v-icon>mdi-file</v-icon><span>파일 업로드</span></v-btn>
+  </div>
 </template>
 
 <script>
@@ -8,6 +16,43 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .xidps-file-container{
+    width:703px;
+    display:flex;
+    justify-content: space-between;
+
+    .xidps-file-result{
+      width:576px;
+      border:1px solid #cbcbcb;
+      background-color:$white;
+
+      .xidps-file-list{
+        height:45px;
+      }
+      .xidps-file-type{
+        width:100%;
+        height:15px;
+        padding:0 6px;
+        background-color:#eeeeee;
+        font-size:$fs7;
+        color:$warm-grey !important;
+
+        span{
+          float:right;
+        }
+      }
+    }
+
+    .xidps-file-btn{
+      border:1px solid #cbcbcb !important;
+      width:111px !important;
+      height:58px !important;
+      font-size:$fs7;
+      font-weight: $semi-bold;
+      color:$warm-grey !important;
+
+    }
+  }
 
 </style>
