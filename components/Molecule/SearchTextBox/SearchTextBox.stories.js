ae2e010b0fdca423c73fc8f31408e9e71bab3f1e
jaehong <println@kakao.com>
2022-07-07 14:11:27 +0900

    Update SearchTextBox
    SearchTextBox 컴포넌트 디자인 변경 및 데이터그리드뷰에서 받아온 속성으로 검색 하는 기능 추가

--- components/Molecule/SearchTextBox/SearchTextBox.stories.js
+++ components/Molecule/SearchTextBox/SearchTextBox.stories.js
@@ -1,14 +1,18 @@
+import SearchTextBox from "@/components/Molecule/SearchTextBox/SearchTextBox";
+import {VApp} from "vuetify/lib/components";
 export default{
-  title:"Molecule/SearchTextBox"
+  title:"Molecule/SearchTextBox",
+  component:SearchTextBox,
 }
 
 const Template = (args,{argsType}) =>({
-  components:{
-    SearchTextBox:()=> import("~/components/Molecule/SearchTextBox/SearchTextBox"),
-    VApp:()=> import("vuetify/lib/components/VApp")
-  },
-
-  template:`<v-app><search-text-box></search-text-box></v-app>`
+  components:{SearchTextBox,VApp},
+  props:Object.keys(args),
+  template:`<v-app><search-text-box v-bind="$props"></search-text-box></v-app>`
 })
 
-export const Sample = Template.bind({});
+export const sample = Template.bind({});
+sample.args = {
+  searchColumns: [{value:"sample",text:"샘플1"}],
+  searchText: ""
+}
--- components/Molecule/SearchTextBox/SearchTextBox.vue
+++ components/Molecule/SearchTextBox/SearchTextBox.vue
@@ -5,11 +5,21 @@
     dense
     type="text"
     hide-details
-    height="30px"
-    class="xidps-search-text"
+    v-model="searchText"
   >
+    <template v-slot:prepend-inner>
+      <v-select v-model="selectedKey"
+                key="value"
+                value="text"
+                :items="searchColumns"
+                height="20px"
+                style="margin-top:2px"
+                hide-details
+                dense>
+      </v-select>
+    </template>
     <template v-slot:append>
-      <v-btn icon  width="12" height="12">
+      <v-btn @click="clickSearchBtn" icon width="16" height="16">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </template>
@@ -19,11 +29,51 @@
 <script>
 export default {
   name: "SearchTextBox",
+  data() {
+    return {
+      selectedKey: ""
+    }
+  },
+  props: {
+    searchColumns: {
+      type: Array,
+      default: function () {
+        return [
+          {text: "", value: ""}
+        ]
+      }
+    },
+    searchText: {
+      type: String,
+      default: function () {
+        return "";
+      }
+    }
+  },
+  methods: {
+    clickSearchBtn() {
+      /*if(this.selectedKey === "") {
+        alert("검색 조건이 선택되지 않았습니다.")
+        return ;
+      }*/
+      this.$emit("search",{
+        searchColumnName:"number",
+        searchText:this.searchText
+      })
+    }
+  },
+  mounted() {
+    console.log(this.searchColumns)
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.xidps-search-bar{
+.xidps-search-bar {
   font-size: $fs8 !important;
+}
+
+::v-deep .v-input__icon--clear {
+  padding-bottom: 6px !important;
 }
 </style>
