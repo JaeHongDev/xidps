d34c866e9363532d0c9829968373faa3da14a3ea
JaehongDev <println@kakao.com>
2022-07-13 11:49:24 +0900

    Create UserExcelLoader Component
    write UserExcelLoader Component , stories file

---
+++ components/Organism/UserExcelLoader/UserExcelLoader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <data-grid-view ></data-grid-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserExcelLoader"
+}
+</script>
+
+<style scoped>
+
+</style>
