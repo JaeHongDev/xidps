b1781496b6bee98648098d32c5b0a4ffd41368ed
JaehongDev <println@kakao.com>
2022-07-13 16:26:17 +0900

    Create ExcelDataViewer Component

--- components/Molecule/ExcelDataViewer/ExcelDataViewer.vue
+++ components/Molecule/ExcelDataViewer/ExcelDataViewer.vue
@@ -1,13 +1,82 @@
 <template>
-$END$
+  <!--
+    <v-data-table
+      :headers='headers'
+      :items='rows'
+
+      disable-sort
+      disable-pagination
+      hide-default-footer
+      dense
+      class='excel-data-viewer'
+    >
+    </v-data-table>
+  -->
+
+  <v-data-table
+    :headers='headers'
+    :items='rows'
+    disable-sort
+    disable-pagination
+    hide-default-footer
+    dense
+    height='158'
+    class='excel-data-viewer'
+  >
+  </v-data-table>
+
 </template>
 
 <script>
 export default {
-name: "ExcelDataViewer"
+  // this component is only possible scroll table
+  name: "ExcelDataViewer",
+  props: {
+    headers: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    rows: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+.excel-data-viewer {
+  border: 1px solid #d6d6d6 !important;
+  border-collapse: collapse;
+  width: 750px !important;
 
+  ::v-deep table {
+    td, th {
+      border: 1px solid #d6d6d6 !important;
+      vertical-align: middle !important;
+      text-align: center;
+
+    }
+
+    td {
+      font-size: $fs5;
+      color: $warm-grey;
+    }
+
+    th {
+
+      color: $light-navy-blue !important;
+      font-size: $semi-bold;
+      border-spacing: 5px;
+    }
+  }
+
+  ::v-deep .v-data-table-header {
+    background-color: #ebebeb !important;
+  }
+}
 </style>
