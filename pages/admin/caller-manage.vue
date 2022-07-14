ef80ca4dcab6768dde212bb9601a47e5795c9e38
JaehongDev <println@kakao.com>
2022-07-14 17:37:44 +0900

    Update caller-manage page

--- pages/admin/caller-manage.vue
+++ pages/admin/caller-manage.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    <caller-manager></caller-manager>
-  </div>
+  <data-grid-view>
+
+  </data-grid-view>
 </template>
 
 <script>
+
+
 export default {
-  name: "CallerManage"
+  name: "CallerManage",
+
+  data() {
+    return {
+      rows: [],
+      headers: [],
+    }
+  },
+  methods: {
+    createDefault({callerNumber, manager, department, status, memo,}) { // 초기 값
+      return {
+        editable: false,
+        division: "INSERT",
+        callerNumber: callerNumber ?? "",
+        manager: manager ?? "",
+        department: department ?? "",
+        status: status ?? "",
+        memo: memo ?? "",
+      }
+    },
+    createEditDefault({manager, departmentmmemo}) { // 편집 할 대상의 초기 값
+      return {
+        manager: manager ?? "",
+        department: department ?? "",
+        memo: memo ?? "",
+      }
+    },
+
+  }
 }
 </script>
 
