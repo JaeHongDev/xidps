4c762119326950537346b31c7e470a67ebe85533
JaehongDev <println@kakao.com>
2022-07-14 13:36:53 +0900

    Add customModal stories case
    customModal 컴포넌트 스트로의 예시 추가

--- components/Molecule/CustomModal/CustomModal.stories.js
+++ components/Molecule/CustomModal/CustomModal.stories.js
@@ -1,5 +1,6 @@
 import CustomModal from "@/components/Molecule/CustomModal/CustomModal";
 import {VApp} from "vuetify/lib/components";
+import UserExcelLoader from "@/components/Organism/UserExcelLoader/UserExcelLoader";
 
 export default {
   title: "Molecule/CustomModal",
@@ -14,7 +15,7 @@ const Template = (args, {argTypes}) => ({
   props: Object.keys(args),
   template: `
     <v-app>
-    <custom-modal v-bind='$props'></custom-modal>
+    <custom-modal v-bind='$props'>${args.content}</custom-modal>
     </v-app>`
 })
 
@@ -23,5 +24,13 @@ sample.args = {
   isShow: true,
   width:700,
   title:"샘플"
+}
+
+export const userUploadModal = Template.bind({});
+userUploadModal.args = {
+  content: `<user-excel-loader></user-excel-loader>`,
+  isShow: true,
+  width:900,
+  title:"엑셀 업로드"
 }
 
