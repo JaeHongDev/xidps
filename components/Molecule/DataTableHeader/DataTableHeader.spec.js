53878684bc9de0aa6f7e6a1e35503ada5dca536c
jaehong <println@kakao.com>
2022-07-22 11:15:25 +0900

    Update DatatTable TestCode

---
+++ components/Molecule/DataTableHeader/DataTableHeader.spec.js
@@ -0,0 +1,29 @@
+import DataTableHeader from "@/components/Molecule/DataTableHeader/DataTableHeader";
+import { shallowMount } from "@vue/test-utils";
+
+describe("DataTableHeader", () => {
+  let DataTableHeaderWrap;
+
+  beforeEach(() => {
+    DataTableHeaderWrap = shallowMount(DataTableHeader);
+  });
+  it("DataTableHeader 컴포넌트가 정상적으로 랜더링 되는가", () => {
+    expect(DataTableHeaderWrap.exists()).toBeTruthy();
+  });
+  it("DataTableHeader 컴포넌트의 props로 title과 count가 전달될 경우 정상적으로 화면에 표시된다.", async () => {
+    await DataTableHeaderWrap.setProps({ title: "테스트제목", count: "1000" });
+
+    const dataTableTitle = DataTableHeaderWrap.find("[data-test='title']");
+    const dataTableCount = DataTableHeaderWrap.find("[data-test='count']");
+
+    expect(dataTableTitle.text()).toBe("테스트제목");
+    expect(dataTableCount.text()).toBe("TOTAL : 1000건");
+  });
+  it("DataTableHeader 컴포넌트의 props로 어떠한 값도 전달되지 않을 경우 default값이 표시되도록 한다.", () => {
+    const dataTableTitle = DataTableHeaderWrap.find("[data-test='title']");
+    const dataTableCount = DataTableHeaderWrap.find("[data-test='count']");
+
+    expect(dataTableTitle.text()).toBe("명칭 없음");
+    expect(dataTableCount.text()).toBe("TOTAL : 0건");
+  });
+});
--- components/Molecule/DataTableHeader/DataTableHeader.vue
+++ components/Molecule/DataTableHeader/DataTableHeader.vue
@@ -1,37 +1,38 @@
 <template>
-    <div>
-      <span class="data-table-header-title fw-semi-bold light-navy-blue fs-3 ">{{title}}</span>
-      <div class="data-table-header-count">
-        <span class=" fs-5 warm-grey fw-regular">TOTAL : {{count}}건</span>
-      </div>
+  <div>
+    <span class="data-table-header-title fw-semi-bold light-navy-blue fs-3 " data-test="title">{{ title }}</span>
+    <div class="data-table-header-count">
+      <span class=" fs-5 warm-grey fw-regular" data-test="count">TOTAL : {{ count }}건</span>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "DataTableHeader",
-  props:{
+  props: {
     title: {
       type: String,
-      default: function () {
-        return "등록되지 않는 명칭"
+      default: function() {
+        return "명칭 없음";
       }
     },
-    count:{
-      type:Number,
-      default:function(){
-        return 0
+    count: {
+      type: Number,
+      default: function() {
+        return 0;
       }
     }
-  },
-}
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  .data-table-header-title{
-    padding-left:3px;
-  }
-  .data-table-header-count{
-    margin-top:13px;
-  }
+.data-table-header-title {
+  padding-left: 3px;
+}
+
+.data-table-header-count {
+  margin-top: 13px;
+}
 </style>
